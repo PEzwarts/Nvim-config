0c11ca61a99bb2bab5c703b75c7cf09c04d7396e
PEzwarts <pe.zwarts@tutamail.com>
2024-06-11 21:56:58 +0200

    Neovim config release!

---
+++ README.md
@@ -0,0 +1,2 @@
+Simple neovim config. Terminal, file manager, lang support.
+
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,39 @@
+require("lazyvim.plugins")
+
+return {
+  {
+    "lazyvim/lazyvim",
+    opts = {
+      colorscheme = "base16-3024",
+    }
+  },
+
+  {
+    "RRethy/base16-nvim",
+  },
+
+  {
+    "akinsho/toggleterm.nvim",
+    tag = "*",
+    config = true,
+    opts = {
+      direction = "float",
+      open_mapping = [[<A-x>]],
+    }
+  },
+
+  { "ast-grep/ast-grep" },
+  { "microsoft/vscode-cpptools" },
+  { "vadimcn/codelldb" },
+  { "aquasecurity/trivy" },
+  { "snyk/cli" },
+
+  { "mrcjkb/rustaceanvim" },
+
+  {
+    "kdheepak/lazygit.nvim",
+    keys = {
+      { "<A-z>", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    },
+  },
+}
