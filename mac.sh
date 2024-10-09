ca69816ed425a799decb6ad49c7ca1371bb9d125
PEzwarts <pe.zwarts@tutamail.com>
2024-10-09 20:18:51 +0000

    Updated mac.sh

--- mac.sh
+++ mac.sh
@@ -7,5 +7,5 @@ chsh -s /usr/local/bin/fish
 
 gsed -i 's/<A-x>/<≈>/g' lua/plugins/plugins.lua
 
-gsed -i 's/<A-z>/<Ω>/g' lua/config/keymaps.lua
-gsed -i 's/<A-c>/<ç>/g' lua/config/keymaps.lua
+gsed -i 's/<A-z>/<Ω>/g' ~/.config/nvim/lua/config/keymaps.lua
+gsed -i 's/<A-c>/<ç>/g' ~/.config/nvim/lua/config/keymaps.lua
