bc6174facc9a71ae5a010b2a777d1c155d532869
PEzwarts <pe.zwarts@tutamail.com>
2024-10-09 20:15:25 +0000

    Updated mac.sh

--- mac.sh
+++ mac.sh
@@ -5,7 +5,7 @@ sudo pacman -S --noconfirm git fish neovim
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
 
-gsed -i 's/<A-x>/<≈>/g' ./lua/plugins/plugins.lua
+gsed -i 's/<A-x>/<≈>/g' lua/plugins/plugins.lua
 
-gsed -i 's/<A-z>/<Ω>/g' ./lua/config/keymaps.lua
-gsed -i 's/<A-c>/<ç>/g' ./lua/config/keymaps.lua
+gsed -i 's/<A-z>/<Ω>/g' lua/config/keymaps.lua
+gsed -i 's/<A-c>/<ç>/g' lua/config/keymaps.lua
