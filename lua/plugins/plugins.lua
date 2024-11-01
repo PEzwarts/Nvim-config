f16b618d24a6b268307dd912db5b703860bda6e5
PEzwarts <pe.zwarts@tutamail.com>
2024-11-01 21:18:06 +0000

    Updated keymaps.lua and plugins.lua

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -14,14 +14,4 @@ return {
       open_mapping = [[<A-x>]],
     },
   },
-
-  { "OXY2DEV/markview.nvim" },
-
-  { "ast-grep/ast-grep" },
-  { "microsoft/vscode-cpptools" },
-  { "vadimcn/codelldb" },
-  { "aquasecurity/trivy" },
-  { "snyk/cli" },
-
-  { "mrcjkb/rustaceanvim" },
 }
